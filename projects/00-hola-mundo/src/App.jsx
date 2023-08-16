9dacbd920dad0903732cddd579aa79b18c9429ea
Rico98 <asd.cqd98@gmail.com>
2023-08-16 17:33:29 -0500

    Initial commit

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/github/mdo" alt="Avatar de mdo" />
+        <div>
+          <strong>Avatar 1</strong>
+          <span>@mdo</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
