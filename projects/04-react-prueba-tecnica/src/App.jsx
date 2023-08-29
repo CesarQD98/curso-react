7268046c712363aa5575e66e28704bdb1747c468
Rico98 <asd.cqd98@gmail.com>
2023-08-29 02:24:53 -0500

    Added button and logic functions

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts'
+import { getRandomImage } from './services/catImages'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+  const [isDisabled, setIsDisabled] = useState(false)
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
+    getRandomFact().then(newFact => setFact(newFact))
   }, [])
 
   useEffect(() => {
@@ -23,21 +21,36 @@ export function App () {
 
     const threeFirstWords = fact.split(' ', 3).join(' ')
 
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
+    getRandomImage(threeFirstWords).then(newImageUrl => setImageUrl(newImageUrl))
   }, [fact])
 
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+    setIsDisabled(true)
+
+    setTimeout(() => {
+      setIsDisabled(false)
+    }, 2000)
+  }
+
   return (
-    <main>
+    <main style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '20px'
+    }}
+    >
       <h1>App de gatitos</h1>
       <section>
         {fact && <p>{fact}</p>}
         {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image obtained using the first three words from ${fact}`} />}
       </section>
+
+      <button disabled={isDisabled} onClick={handleClick}>
+        {isDisabled ? 'Loading...' : 'Get a new fact!'}
+      </button>
     </main>
   )
 }
