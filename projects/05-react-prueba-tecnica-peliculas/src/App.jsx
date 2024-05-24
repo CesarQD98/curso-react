00bb1e3c40927ffc4817ccda546ba0dc629fc5d4
Rico98 <asd.cqd98@gmail.com>
2024-05-24 14:17:57 -0500

    feat: add movies tech test

---
+++ projects/05-react-prueba-tecnica-peliculas/src/App.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from 'react'
+import './App.css'
+import Movies from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+
+function useSearch () {
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, setSearch, error }
+}
+
+function App () {
+  const [sort, setSort] = useState(false)
+  const { search, setSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies()
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    setSearch(newSearch)
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  return (
+    <div className='page'>
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={search} name='search' placeholder='Avengers, Ice Age, The Matrix ...' />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ projects/05-react-prueba-tecnica-peliculas/src/components/Movies.jsx
@@ -0,0 +1,29 @@
+const ListOfMovies = ({ movies }) => {
+  return (
+    <ul className='movies'>
+      {
+        movies.map(movie => (
+          <li className='movie' key={movie.id}>
+            <h3>{movie.title}</h3>
+            <p>{movie.year}</p>
+            <img src={movie.poster} alt={movie.title} />
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+const NoResults = () => {
+  return (
+    <p>No se encontraron resultados</p>
+  )
+}
+
+export default function Movies ({ movies }) {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies ? <ListOfMovies movies={movies} /> : <NoResults />
+  )
+}
